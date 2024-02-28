6af0b72207da7e1304bd19a8d3aaa40ee7e6238b
ploot <26153075+uwuplant@users.noreply.github.com>
2024-02-28 18:03:35 -0500

    h

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -41,6 +41,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 
             double aimRating = Math.Pow(skills[0].DifficultyValue(), AIM_EXP) * aim_scaling;
             double[] aimMisscounts = ((Aim)skills[0]).GetMissCountsForPenalty();
+            Console.WriteLine(aimMisscounts);
             double speedRating = Math.Pow(skills[1].DifficultyValue(), tap_exp) * tap_scaling;
             double speedNotes = ((Speed)skills[1]).RelevantNoteCount();
 
