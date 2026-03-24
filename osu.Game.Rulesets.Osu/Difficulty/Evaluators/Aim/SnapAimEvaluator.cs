f3df159515e33eb462fe2485dc3d734ab4af8a3d
ploot <26153075+uwuplant@users.noreply.github.com>
2026-03-24 09:47:45 -0400

    try something

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/SnapAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/SnapAimEvaluator.cs
@@ -13,7 +13,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
     public static class SnapAimEvaluator
     {
         private const double wide_angle_multiplier = 1.05;
-        private const double acute_angle_multiplier = 2.41;
+        private const double acute_angle_multiplier = 2.3;
         private const double slider_multiplier = 1.5;
         private const double velocity_change_multiplier = 0.9;
         private const double wiggle_multiplier = 1.02; // WARNING: Increasing this multiplier beyond 1.02 reduces difficulty as distance increases. Refer to the desmos link above the wiggle bonus calculation
